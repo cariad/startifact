94bbba697fd6e9295277ef7ea9eb4fc135c627fa
Cariad Eccleston <cariad@cariad.earth>
2021-12-12 17:14:17 +0000

    WIP

--- startifact/session.py
+++ startifact/session.py
@@ -1,149 +1,143 @@
 from logging import getLogger
 from pathlib import Path
 from re import match
-from typing import Dict, Optional, Union
+from sys import stdout
+from typing import IO, Dict, List, Optional
 
-import boto3.session
+from semver import VersionInfo  # pyright: reportMissingTypeStubs=false
 
-from startifact.account import Account
-from startifact.artifact import NewArtifact, StagedArtifact
-from startifact.enums import SessionUsage
-from startifact.exceptions import NoConfiguration, ProjectNameError
-from startifact.parameters import (
-    BucketParameter,
-    ConfigurationParameter,
-    LatestVersionParameter,
-)
-from startifact.types import Configuration
+from startifact.artifact.abc import Artifact
+from startifact.artifact.new import Stager
+from startifact.configuration_loader import ConfigurationLoader
+from startifact.exceptions import CannotStageArtifact, ProjectNameError
+from startifact.regions import get_regions
 
 
 class Session:
     """
     A Startifact session.
+
+    Configuration will be cached during this session, so reuse it when possible.
+
+    Arguments:
+        out: stdout proxy. Defaults to stdout.
+
+        read_only: Prevents the session writing to Amazon Web Services.
+
+        regions: Regions to operate in. Defaults to reading your
+        STARTIFACT_REGIONS environment variable.
     """
 
-    def __init__(self, dry_run: bool = False) -> None:
-        self._cached_account: Optional[Account] = None
-        self._cached_bucket_name: Optional[str] = None
-        self._cached_configuration: Optional[Configuration] = None
-        self._cached_sessions: Dict[SessionUsage, boto3.session.Session] = {}
-        self._cached_session_regions: Dict[SessionUsage, str] = {}
-        self._dry_run = dry_run
+    def __init__(
+        self,
+        configuration_loader: Optional[ConfigurationLoader] = None,
+        out: Optional[IO[str]] = None,
+        read_only: bool = False,
+        regions: Optional[List[str]] = None,
+    ) -> None:
+
+        self._cached_regions = regions
+        self._cached_configuration_loader = configuration_loader
+        self._read_only = read_only
         self._logger = getLogger("startifact")
+        self._out = out or stdout
 
     @property
-    def _account(self) -> Account:
+    def configuration_loader(self) -> ConfigurationLoader:
         """
-        Gets the Amazon Web Services account.
+        Gets the configuration loader.
         """
 
-        if self._cached_account is None:
-            session = self._get_session(SessionUsage.DEFAULT)
-            self._cached_account = Account(session)
-        return self._cached_account
-
-    @property
-    def _configuration(self) -> Configuration:
-        if self._cached_configuration is None:
-            session = self._get_session(SessionUsage.DEFAULT)
-            param = ConfigurationParameter(
-                account=self._account,
-                dry_run=self._dry_run,
-                session=session,
+        if not self._cached_configuration_loader:
+            self._cached_configuration_loader = ConfigurationLoader(
+                out=self._out,
+                regions=self.regions,
             )
-            self._cached_configuration = param.value
-        return self._cached_configuration
 
-    def _get_session(self, usage: SessionUsage) -> boto3.session.Session:
-        if usage not in self._cached_sessions:
-            self._logger.debug("Creating Boto3 session for %s.", usage)
-            r = (
-                None
-                if usage is SessionUsage.DEFAULT
-                else self.session_regions.get(usage, None)
-            )
-            s = boto3.session.Session(region_name=r) if r else boto3.session.Session()
-            self._cached_sessions[usage] = s
-        return self._cached_sessions[usage]
+        return self._cached_configuration_loader
 
-    def _latest_param(self, project: str) -> LatestVersionParameter:
-        return LatestVersionParameter(
-            account=self._account,
-            dry_run=self._dry_run,
-            prefix=self._configuration["parameter_name_prefix"],
+    def get(self, project: str, version: Optional[VersionInfo] = None) -> Artifact:
+        """
+        Gets an artifact.
+
+        :param project: Project.
+        :type project: str
+
+        :param version: Version. Omit to infer the latest version.
+        :type version: Optional[VersionInfo]
+
+        :returns: Artifact.
+        """
+
+        config = self.configuration_loader.loaded
+
+        return Artifact(
+            bucket_name_parameter=config["bucket_name_param"],
+            out=self._out,
+            parameter_name_prefix=config["parameter_name_prefix"],
             project=project,
-            session=self._get_session(SessionUsage.SSM_FOR_ARTIFACTS),
+            regions=self.regions,
+            s3_key_prefix=config["bucket_key_prefix"],
+            version=version,
+        )
+
+    def make_stager(
+        self,
+        path: Path,
+        project: str,
+        version: VersionInfo,
+        metadata: Optional[Dict[str, str]] = None,
+    ) -> Stager:
+        """
+        Creates and returns an artifact stager.
+        """
+
+        config = self.configuration_loader.loaded
+
+        return Stager(
+            bucket_name_param=config["bucket_name_param"],
+            metadata=metadata,
+            out=self._out,
+            parameter_name_prefix=config["parameter_name_prefix"],
+            path=path,
+            project=project,
+            read_only=self.read_only,
+            regions=self.regions,
+            s3_key_prefix=config["bucket_key_prefix"],
+            version=version,
         )
 
     @property
-    def bucket(self) -> str:
+    def read_only(self) -> bool:
         """
-        Gets the artifacts bucket name.
-
-        Raises startifact.exceptions.NoConfiguration if the organisation
-        configuration is not set.
+        Returns `True` if this session is read-only.
         """
 
-        if self._cached_bucket_name is None:
-            if not self._configuration["bucket_param_name"]:
-                raise NoConfiguration("bucket_param_name")
-
-            session = self._get_session(SessionUsage.SSM_FOR_BUCKET)
-
-            param = BucketParameter(
-                account=self._account,
-                dry_run=self._dry_run,
-                name=self._configuration["bucket_param_name"],
-                session=session,
-            )
-
-            self._cached_bucket_name = param.value
-
-        return self._cached_bucket_name
+        return self._read_only
 
     @property
-    def dry_run(self) -> bool:
-        return self._dry_run
+    def regions(self) -> List[str]:
+        """
+        Gets the regions that this session will operate in.
+        """
 
-    def get(self, project: str, version: str = "latest") -> StagedArtifact:
-        """Gets an artifact."""
-
-        return StagedArtifact(
-            bucket=self.bucket,
-            dry_run=self._dry_run,
-            key_prefix=self._configuration["bucket_key_prefix"],
-            project=project,
-            session=self._get_session(SessionUsage.S3),
-            version=self.latest(project) if "latest" else version,
-        )
-
-    def latest(self, project: str) -> str:
-        """Gets the latest version number of an artifact."""
-
-        param = self._latest_param(project)
-        self._logger.debug("%s@latest = %s", project, param.value)
-        return param.value
-
-    @property
-    def session_regions(self) -> Dict[SessionUsage, str]:
-        if not self._cached_session_regions:
-            self._cached_session_regions = {
-                SessionUsage.S3: self._configuration["bucket_region"],
-                SessionUsage.SSM_FOR_ARTIFACTS: self._configuration["parameter_region"],
-                SessionUsage.SSM_FOR_BUCKET: self._configuration["bucket_param_region"],
-            }
-        return self._cached_session_regions
+        if self._cached_regions is None:
+            self._cached_regions = get_regions()
+        return self._cached_regions
 
     def stage(
         self,
         project: str,
-        version: str,
-        path: Union[Path, str],
-        dry_run: bool = False,
+        version: VersionInfo,
+        path: Path,
         metadata: Optional[Dict[str, str]] = None,
-    ) -> StagedArtifact:
+    ) -> None:
         """
         Stages an artifact.
+
+        Returns:
+            `True` if the artifact is staged to at least one region. `False` if
+            the artifact could not be staged anywhere.
 
         Raises `startifact.exceptions.ProjectNameError` if the project name is
         not acceptable.
@@ -154,37 +148,15 @@ class Session:
 
         self.validate_project_name(project)
 
-        s3_session = self._get_session(SessionUsage.S3)
-
-        artifact = NewArtifact(
-            bucket=self.bucket,
-            dry_run=self._dry_run,
-            key_prefix=self._configuration["bucket_key_prefix"],
+        stager = self.make_stager(
+            path=path,
             project=project,
-            session=s3_session,
             version=version,
+            metadata=metadata,
         )
 
-        artifact.upload(path)
-
-        if metadata:
-            for key in metadata:
-                self._logger.debug("Setting metadata: %s = %s", key, metadata[key])
-                artifact[key] = metadata[key]
-            artifact.save_metadata()
-
-        param = self._latest_param(project)
-
-        param.set(version)
-
-        return StagedArtifact(
-            bucket=self.bucket,
-            dry_run=self._dry_run,
-            key_prefix=self._configuration["bucket_key_prefix"],
-            project=project,
-            session=s3_session,
-            version=version,
-        )
+        if not stager.stage():
+            raise CannotStageArtifact("Could not stage to any regions.")
 
     @staticmethod
     def validate_project_name(name: str) -> None:
--- tests/test_session.py
+++ tests/test_session.py
@@ -1,232 +1,143 @@
-from mock import Mock
-from mock.mock import patch
-from pytest import mark, raises
+from io import StringIO
+from pathlib import Path
 
-from startifact.account import Account
-from startifact.artifact import NewArtifact
-from startifact.enums import SessionUsage
-from startifact.exceptions import NoConfiguration, ProjectNameError
-from startifact.session import Session
-from startifact.types import Configuration
+from _pytest.monkeypatch import MonkeyPatch
+from mock import patch
+from mock.mock import Mock
+from pytest import raises
+from semver import VersionInfo  # pyright: reportMissingTypeStubs=false
 
-# pyright: reportPrivateUsage=false
-
-
-def test_account() -> None:
-    boto_session = Mock()
-
-    session = Session()
-    session._cached_sessions[SessionUsage.DEFAULT] = boto_session
-    actual1 = session._account
-    actual2 = session._account
-
-    assert actual1.session is boto_session
-    assert actual1 is actual2
+from startifact import Session
+from startifact.artifact.abc import Artifact
+from startifact.artifact.new import Stager
+from startifact.configuration_loader import ConfigurationLoader
+from startifact.exceptions import CannotStageArtifact, ProjectNameError
 
 
-def test_bucket(empty_config: Configuration) -> None:
-    empty_config["bucket_param_name"] = "bp_name"
-
-    boto_session = Mock()
-
-    session = Session()
-    session._cached_account = Account(session=Mock(), account_id="000000000000")
-    session._cached_configuration = empty_config
-    session._cached_sessions[SessionUsage.SSM_FOR_BUCKET] = boto_session
-
-    bp = Mock()
-    bp.value = "foo"
-
-    with patch("startifact.session.BucketParameter") as bp_class:
-        bp_class.return_value = bp
-
-        # Intentionally hit the property twice so we can assert the bucket
-        # parameter is hit only once.
-        bucket1 = session.bucket
-        bucket2 = session.bucket
-
-    bp_class.assert_called_once_with(
-        account=session._cached_account,
-        dry_run=False,
-        name="bp_name",
-        session=boto_session,
+def test_configuration_loader(out: StringIO) -> None:
+    session = Session(
+        out=out,
+        regions=["us-east-3"],
     )
 
-    assert bucket1 == bucket2 == "foo"
+    loader = session.configuration_loader
+
+    assert loader.out is out
+    assert loader.regions == ["us-east-3"]
 
 
-def test_bucket__no_configuration(empty_config: Configuration) -> None:
-    session = Session()
-    session._cached_configuration = empty_config
+def test_get(configuration_loader: ConfigurationLoader, out: StringIO) -> None:
+    configuration_loader.loaded["bucket_key_prefix"] = "bucket-key-prefix"
+    configuration_loader.loaded["bucket_name_param"] = "bucket-name-param"
+    configuration_loader.loaded["parameter_name_prefix"] = "parameter-name-prefix"
 
-    with raises(NoConfiguration):
-        session.bucket
-
-
-def test_config(empty_config: Configuration) -> None:
-    session = Session()
-    boto_session = Mock()
-    session._cached_account = Account(session=Mock(), account_id="000000000000")
-    session._cached_sessions[SessionUsage.DEFAULT] = boto_session
-
-    cp = Mock()
-    cp.value = empty_config
-
-    with patch("startifact.session.ConfigurationParameter") as cp_class:
-        cp_class.return_value = cp
-        config = session._configuration
-
-    cp_class.assert_called_once_with(
-        account=session._cached_account,
-        dry_run=False,
-        session=boto_session,
+    session = Session(
+        configuration_loader=configuration_loader,
+        out=out,
+        regions=["us-east-3"],
     )
 
-    assert config is empty_config
+    # with patch.object(session, "latest", return_value="1.2.3") as latest:
+    artifact = session.get("SugarWater", VersionInfo(1, 2, 3))
 
-
-def test_get(empty_config: Configuration) -> None:
-    session = Session()
-    session._cached_bucket_name = "ArtifactsBucket"
-    session._cached_configuration = empty_config
-    session._cached_sessions[SessionUsage.DEFAULT] = Mock()
-
-    with patch.object(session, "latest", return_value="1.2.3") as latest:
-        artifact = session.get("SugarWater")
-
-    latest.assert_called_once_with("SugarWater")
-
-    assert artifact.project == "SugarWater"
-    assert artifact.version == "1.2.3"
-
-
-def test_latest(empty_config: Configuration) -> None:
-    empty_config["parameter_name_prefix"] = "/prefix"
-
-    boto_session = Mock()
-
-    session = Session()
-    session._cached_account = Account(session=Mock(), account_id="000000000000")
-    session._cached_configuration = empty_config
-    session._cached_sessions[SessionUsage.SSM_FOR_ARTIFACTS] = boto_session
-
-    lvp = Mock()
-    lvp.value = "1.2.3"
-
-    with patch("startifact.session.LatestVersionParameter") as lvp_class:
-        lvp_class.return_value = lvp
-        version = session.latest("SugarWater")
-
-    lvp_class.assert_called_once_with(
-        account=session._cached_account,
-        dry_run=False,
-        prefix="/prefix",
+    expect = Artifact(
+        bucket_name_parameter="bucket-name-param",
+        out=out,
+        parameter_name_prefix="parameter-name-prefix",
         project="SugarWater",
-        session=boto_session,
+        regions=["us-east-3"],
+        s3_key_prefix="bucket-key-prefix",
+        version=VersionInfo(1, 2, 3),
     )
 
-    assert version == "1.2.3"
+    assert artifact == expect
 
 
-def test_session_regions(empty_config: Configuration) -> None:
-    empty_config["bucket_region"] = "us-east-1"
-    empty_config["bucket_param_region"] = "us-east-2"
-    empty_config["parameter_region"] = "us-east-3"
+def test_make_stager(configuration_loader: ConfigurationLoader, out: StringIO) -> None:
+    configuration_loader.loaded["bucket_key_prefix"] = "bucket-key-prefix"
+    configuration_loader.loaded["bucket_name_param"] = "bucket-name-param"
+    configuration_loader.loaded["parameter_name_prefix"] = "parameter-name-prefix"
 
-    session = Session()
-    session._cached_configuration = empty_config
+    session = Session(
+        configuration_loader=configuration_loader,
+        out=out,
+        regions=["us-east-7"],
+    )
 
-    assert session.session_regions == {
-        SessionUsage.S3: "us-east-1",
-        SessionUsage.SSM_FOR_ARTIFACTS: "us-east-3",
-        SessionUsage.SSM_FOR_BUCKET: "us-east-2",
-    }
-
-    assert session.session_regions == session.session_regions
-
-
-def test_stage(empty_config: Configuration) -> None:
-    artifact = NewArtifact(
-        bucket="",
-        dry_run=False,
-        key_prefix="",
+    stager = session.make_stager(
+        path=Path("foo.zip"),
         project="SugarWater",
-        session=Mock(),
-        version="1.2.3",
+        version=VersionInfo(1, 2, 3),
     )
 
-    s3_session = Mock()
-
-    session = Session()
-    session._cached_bucket_name = "ArtifactsBucket"
-    session._cached_configuration = empty_config
-    session._cached_sessions[SessionUsage.S3] = s3_session
-
-    param_set = Mock()
-    param = Mock()
-    param.set = param_set
-
-    with patch("startifact.session.NewArtifact", return_value=artifact) as new_artifact:
-        with patch.object(session, "_latest_param", return_value=param) as latest_param:
-            with patch.object(artifact, "upload") as upload:
-                session.stage("SugarWater", "1.2.3", path="README.md")
-
-    new_artifact.assert_called_with(
-        bucket="ArtifactsBucket",
-        dry_run=False,
-        key_prefix="",
+    expect = Stager(
+        bucket_name_param="bucket-name-param",
+        out=out,
+        parameter_name_prefix="parameter-name-prefix",
+        path=Path("foo.zip"),
         project="SugarWater",
-        session=s3_session,
-        version="1.2.3",
-    )
-    upload.assert_called_once_with("README.md")
-    latest_param.assert_called_once_with("SugarWater")
-    param_set.assert_called_once_with("1.2.3")
-
-
-def test_stage__invalid_name() -> None:
-    with raises(ProjectNameError):
-        Session().stage("Sugar Water", "1.2.3", path="README.md")
-
-
-def test_stage__with_metadata(empty_config: Configuration) -> None:
-    artifact = NewArtifact(
-        bucket="",
-        dry_run=False,
-        key_prefix="",
-        project="SugarWater",
-        session=Mock(),
-        version="1.2.3",
+        read_only=False,
+        regions=["us-east-7"],
+        s3_key_prefix="bucket-key-prefix",
+        version=VersionInfo(1, 2, 3),
     )
 
+    assert stager == expect
+
+
+def test_regions(monkeypatch: MonkeyPatch) -> None:
+    monkeypatch.setenv("STARTIFACT_REGIONS", "us-east-7")
+    assert Session().regions == ["us-east-7"]
+
+
+def test_stager() -> None:
     session = Session()
-    session._cached_bucket_name = "ArtifactsBucket"
-    session._cached_configuration = empty_config
 
-    with patch("startifact.session.NewArtifact", return_value=artifact):
-        with patch.object(session, "_latest_param"):
-            with patch.object(artifact, "upload"):
-                with patch.object(artifact, "save_metadata") as save_metadata:
-                    session.stage(
-                        "SugarWater",
-                        "1.2.3",
-                        metadata={"foo": "bar"},
-                        path="README.md",
-                    )
+    stager = Mock()
 
-    save_metadata.assert_called_once_with()
-    assert artifact["foo"] == "bar"
+    stage = Mock(return_value=True)
+    stager.stage = stage
 
+    with patch.object(session, "make_stager", return_value=stager) as make_stager:
+        session.stage("SugarWater", VersionInfo(1, 2, 3), Path("foo.zip"))
 
-@mark.parametrize("name", ["foo"])
-def test_validate_project_name__ok(name: str) -> None:
-    Session.validate_project_name(name)
-    assert True
+    make_stager.assert_called_once_with(
+        path=Path("foo.zip"),
+        project="SugarWater",
+        version=VersionInfo(1, 2, 3),
+        metadata=None,
+    )
+
+    stage.assert_called_once_with()
 
 
-@mark.parametrize("name", ["", " ", "foo "])
-def test_validate_project_name__fail(name: str) -> None:
+def test_stager__fail() -> None:
+    session = Session()
+
+    stager = Mock()
+
+    stage = Mock(return_value=False)
+    stager.stage = stage
+
+    with patch.object(session, "make_stager", return_value=stager) as make_stager:
+        with raises(CannotStageArtifact) as ex:
+            session.stage("SugarWater", VersionInfo(1, 2, 3), Path("foo.zip"))
+
+    make_stager.assert_called_once_with(
+        path=Path("foo.zip"),
+        project="SugarWater",
+        version=VersionInfo(1, 2, 3),
+        metadata=None,
+    )
+
+    stage.assert_called_once_with()
+    assert str(ex.value) == "Could not stage to any regions."
+
+
+def test_stager__invalid_name() -> None:
+    session = Session()
+
     with raises(ProjectNameError) as ex:
-        Session.validate_project_name(name)
-    expect = f'artifact name "{name}" does not satisfy "^[a-zA-Z0-9_\\-\\.]+$"'
-    assert str(ex.value) == expect
+        session.stage("@", VersionInfo(1, 2, 3), Path("foo.zip"))
+
+    assert str(ex.value) == 'Project name "@" does not satisfy "^[a-zA-Z0-9_\\-\\.]+$"'
