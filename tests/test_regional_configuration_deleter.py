ee2af0f101463a249613f301f5b8efc71390c794
Cariad Eccleston <cariad@cariad.earth>
2021-12-13 10:26:59 +0000

    WIP

---
+++ tests/test_regional_configuration_deleter.py
@@ -0,0 +1,20 @@
+from startifact.regional_configuration_deleter import RegionalConfigurationDeleter
+from mock import Mock
+
+def test(session: Mock) -> None:
+    delete_parameter = Mock()
+
+    ssm = Mock()
+    ssm.delete_parameter = delete_parameter
+
+    session.client = Mock(return_value=ssm)
+
+    deleter = RegionalConfigurationDeleter(
+        queue=Mock(),
+        read_only=False,
+        session=session,
+    )
+
+    deleter.operate()
+
+    delete_parameter.assert_called_once_with(Name="/startifact")
