cf669459be7f974a4bb4526b7535e2bcb65e9c16
Cariad Eccleston <cariad@cariad.earth>
2021-12-15 14:45:17 +0000

    WIP

--- tests/test_stager.py
+++ tests/test_stager.py
@@ -4,19 +4,20 @@ from pathlib import Path
 
 from mock import Mock, patch
 from pytest import fixture
-from semver import VersionInfo
+from semver import VersionInfo  # pyright: reportMissingTypeStubs=false
+from startifact import BucketNames
 
 from startifact.regional_process_result import RegionalProcessResult
-from startifact.regional_stager import (  # pyright: reportMissingTypeStubs=false
-    RegionalStager,
+from startifact.regional_stager import (
+    RegionalStager
 )
 from startifact.stager import Stager
 
 
 @fixture
-def stager(out: StringIO, queue: "Queue[RegionalProcessResult]") -> Stager:
+def stager(bucket_names: BucketNames, out: StringIO, queue: "Queue[RegionalProcessResult]") -> Stager:
     return Stager(
-        bucket_name_parameter_name="/bucket-name",
+        bucket_names=bucket_names,
         file_hash="who knows?",
         key="SugarWater@1.2.3",
         out=out,
@@ -24,7 +25,7 @@ def stager(out: StringIO, queue: "Queue[RegionalProcessResult]") -> Stager:
         project="SugarWater",
         queue=queue,
         read_only=True,
-        regions=["us-central-9", "us-central-10", "us-central-11"],
+        regions=["eu-west-10","eu-west-11", "eu-west-12"],
         version=VersionInfo(1, 2, 3),
     )
 
@@ -32,14 +33,15 @@ def stager(out: StringIO, queue: "Queue[RegionalProcessResult]") -> Stager:
 def test_enqueue(session: Mock, stager: Stager) -> None:
     assert not stager.regions_in_progress
     stager.enqueue(session)
-    assert stager.regions_in_progress == ["eu-west-2"]
+    assert stager.regions_in_progress == ["eu-west-10"]
 
 
 def test_make_regional_stager(session: Mock, stager: Stager) -> None:
     regional = stager.make_regional_stager(session)
 
-    assert regional.bucket_name_parameter.name == "/bucket-name"
+    assert regional.bucket == "bucket-10"
     assert regional.file_hash == "who knows?"
+    assert regional.key == "SugarWater@1.2.3"
 
 
 def test_receive_done(
@@ -55,7 +57,7 @@ def test_receive_done(
 
     stager.receive_done()
     assert not stager.regions_in_progress
-    assert out.getvalue() == "🧁 Staged (not really) to eu-west-2.\n"
+    assert out.getvalue() == "🧁 Staged (not really) to eu-west-10.\n"
 
 
 def test_receive_done__error(
@@ -74,7 +76,7 @@ def test_receive_done__error(
 
     stager.receive_done()
     assert not stager.regions_in_progress
-    assert out.getvalue() == "🔥 Failed to stage to eu-west-2: fire\n"
+    assert out.getvalue() == "🔥 Failed to stage to eu-west-10: fire\n"
 
 
 def test_receive_done__none(out: StringIO, stager: Stager) -> None:
@@ -91,9 +93,9 @@ def test_stage(stager: Stager, out: StringIO) -> None:
         ):
             stager.stage()
 
-    expect = """🧁 Staged (not really) to us-central-11.
-🧁 Staged (not really) to us-central-10.
-🧁 Staged (not really) to us-central-9.
+    expect = """🧁 Staged (not really) to eu-west-10.
+🧁 Staged (not really) to eu-west-11.
+🧁 Staged (not really) to eu-west-12.
 """
 
     assert out.getvalue() == expect
